b7df4135491531f8d61e2b1756417a9df089950d
류상욱 <ryuinkyoto@gmail.com>
2020-09-17 14:49:44 +0900

    초기세팅완료

---
+++ service_front/src/routes/index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Main from '../views/Main/Main.vue'
+import Login from '../views/Login/Login.vue'
+import Signup from '../views/Signup/Signup.vue'
+import ProductList from '../views/ProductList/ProductList.vue'
+import ProductDetail from '../views/ProductDetail/ProductDetail.vue'
+import Cart from '../views/Cart/Cart.vue'
+import Order from '../views/Order/Order.vue'
+import Mypage from '../views/Mypage/Mypage.vue'
+import Event from '../views/Event/Event.vue'
+
+
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    mode: "history",
+    routes: [
+        {
+            path: '/main',
+            name: 'Main',
+            component: Main
+        },
+        {
+            path: '/login',
+            name: 'Login',
+            component: Login
+        },
+        {
+            path: '/signup',
+            name: 'Signup',
+            component: Signup
+        },
+        {
+            path: '/productList',
+            name: 'ProductList',
+            component: ProductList
+        },
+        {
+            path: '/productDetail',
+            name: 'ProductDetail',
+            component: ProductDetail
+        },
+        {
+            path: '/cart',
+            name: 'Cart',
+            component: Cart
+        },
+        {
+            path: '/order',
+            name: 'Order',
+            component: Order
+        },
+        {
+            path: '/mypage',
+            name: 'Mypage',
+            component: Mypage
+        },
+        {
+            path: '/event',
+            name: 'Event',
+            component: Event
+        },
+    ]
+})
---
+++ service_front/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+    output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ]
+};
