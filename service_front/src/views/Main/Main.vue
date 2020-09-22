fb4f712f4a09d9503e5216df9743c432d1c3740b
Taegyu Han <65657197+recurian1058@users.noreply.github.com>
2020-09-22 13:36:54 +0900

    [한태규]Add:main페이지 기본 레이아웃 완료 (#4)
    -listCard 컴포넌트 생성후 main페이지와 연결 성공

--- service_front/src/views/Main/Main.vue
+++ service_front/src/views/Main/Main.vue
@@ -1,13 +1,209 @@
 <template>
-  
+  <div id="Main">
+    <header id="rollingBannerContainer">
+      <a href="?">
+        <img
+          alt="rollingBanner"
+          src="http://image.brandi.me/home/banner/bannerImage_159982_1593396258.jpg"
+        />
+      </a>
+      <div id="slideButton">
+        <div class="spot" href="?"></div>
+      </div>
+    </header>
+    <main id="mainListContainer">
+      <div id="mainPageTittle">
+        <span>당신을 위한 추천</span>
+      </div>
+      <div class="MainList">
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+      </div>
+      <div class="mainSubBanner">
+        <a href="?">
+          <img
+            alt="Banner photo"
+            src="http://image.brandi.me/home/banner/bannerImage_159429_1593395957.jpg"
+          />
+        </a>
+      </div>
+      <div class="MainList">
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+      </div>
+      <div class="mainSubBanner">
+        <a href="?">
+          <img
+            alt="Banner photo"
+            src="http://image.brandi.me/home/banner/bannerImage_159375_1593395565.jpg"
+          />
+        </a>
+      </div>
+      <div id="MainListBottom">
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+        <listCard />
+      </div>
+      <div class="buttonContainer">
+        <button class="more">더보기</button>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {
+import listCard from "./MainComponents/ListCard.vue";
 
-}
+export default {
+  name: "main",
+  components: { listCard },
+  data: () => ({}),
+  methods: {},
+};
 </script>
 
-<style>
+<style lang="scss">
+#main {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  box-sizing: border-box;
 
+  #rollingBannerContainer {
+    position: relative;
+    margin-bottom: 30px;
+    padding-bottom: 24px;
+
+    img {
+      max-width: 100%;
+      background-color: black;
+    }
+
+    #slideButton {
+      position: absolute;
+      display: flex;
+      justify-content: center;
+      align-items: flex-end;
+      bottom: 0;
+      width: 100%;
+      height: 19px;
+
+      .spot {
+        display: inline-block;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background-color: black;
+        cursor: pointer;
+      }
+    }
+  }
+
+  #mainListContainer {
+    display: flex;
+    flex-direction: column;
+    width: 1300px;
+    padding: 0 30px;
+    margin: auto;
+
+    .MainList,
+    #MainListBottom {
+      display: flex;
+      flex-wrap: wrap;
+    }
+  }
+
+  #mainPageTittle {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 30px;
+    padding-top: 40px;
+    span {
+      font-size: 26px;
+      font-weight: 700;
+    }
+  }
+
+  .mainSubBanner {
+    height: 313.3px;
+    margin-top: 50px;
+    margin-bottom: 80px;
+
+    img {
+      width: 100%;
+      height: 313.3;
+      padding: 0 6.1875px;
+    }
+  }
+
+  .buttonContainer {
+    display: flex;
+    justify-content: center;
+    height: 82px;
+    padding: 20px 0;
+
+    .more {
+      width: 250px;
+      border: 1px solid black;
+      font-size: 14px;
+
+      &:focus {
+        border: inherit;
+      }
+    }
+  }
+}
 </style>
---
+++ service_front/src/views/Main/MainComponents/ListCard.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="ListCard">
+    <a href="?">
+      <ul>
+        <li>
+          <img
+            alt="product photo"
+            src="http://image.brandi.me/cproduct/2020/08/27/17865870_1598496166_image1_M.jpg"
+          />
+        </li>
+        <li class="listSeller">
+          냥냥 마켓
+          <img alt="One day delivery" src="src/assets/oneday.png" />
+        </li>
+        <li class="productName">안경냥이</li>
+        <li>
+          <span class="discount">9%</span>
+          <span class="price">90</span>
+          <span class="originPrice">100</span>
+        </li>
+        <li class="listCount">5억개 구매중</li>
+      </ul>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "listCard",
+  components: {},
+  data: () => ({}),
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+#ListCard {
+  width: 248px;
+  padding: 0 6.188px 30px;
+
+  img {
+    width: 100%;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  .listSeller {
+    display: flex;
+    justify-content: space-between;
+    font-size: 15.6px;
+    color: #4a4a4a;
+    margin-top: 8px;
+
+    img {
+      width: 50px;
+    }
+  }
+
+  .productName {
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+  .price,
+  .discount {
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  .discount {
+    color: #ff1f4b;
+  }
+
+  .originPrice {
+    font-size: 15px;
+    color: #757575;
+    text-decoration: line-through;
+  }
+
+  .listCount {
+    font-size: 13px;
+    margin-top: 10px;
+  }
+}
+</style>
