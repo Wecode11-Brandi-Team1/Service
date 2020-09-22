e4b12193faa8672b529fbe206b2d15ca326e828d
류상욱 <ryuinkyoto@gmail.com>
2020-09-22 13:36:59 +0900

    Add: 카카오 우편번호 검색서비스 추가

---
+++ service_front/src/components/Order/OrderInfo.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="order-info-wrap">
+    <h2 class="order-info-title">브랜디 배송 상품</h2>
+    <div class="order-info-table">
+      <div class="order-row">
+        <span class="normal-delivery">
+          <img
+            alt="normal delivery"
+            src="https://web-staging.brandi.co.kr/static/2020.7.3/images/ic-seller-xl@3x.png"
+          />
+          상품
+        </span>
+        <span></span>
+      </div>
+      <div class="order-row">
+        <span class="brand">{{ orderlist.brand }}</span>
+        <span class="price">주문금액</span>
+      </div>
+      <div class="order-row">
+        <div class="product-wrap">
+          <span class="product-img">
+            <img alt="product image" :src="orderlist.img" />
+          </span>
+          <span class="product-info">
+            <ul>
+              <li>{{ orderlist.name }}</li>
+              <li class="grey">{{ orderlist.option }}</li>
+              <li class="grey">{{ orderlist.count }}개</li>
+            </ul>
+          </span>
+        </div>
+        <span class="price-value"
+          >{{ (orderlist.price * orderlist.count).toLocaleString() }}원</span
+        >
+      </div>
+    </div>
+    <div class="total-price-wrap">
+      <span class="total-price">총 주문금액&nbsp;</span>
+      <span class="total-price-value"
+        >{{ (orderlist.price * orderlist.count).toLocaleString() }}원</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["orderlist"],
+};
+</script>
+
+<style lang="scss">
+.order-info-wrap {
+  .order-info-title {
+    margin-top: 25px;
+    padding-bottom: 20px;
+    font-size: 1.8em;
+    font-weight: bold;
+    border-bottom: 1px solid black;
+  }
+  .order-info-table {
+    border-bottom: 1px solid black;
+    span {
+      margin: auto 0;
+      padding: 18px;
+    }
+    .order-row {
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #e1e1e1;
+      .quick-delivery,
+      .normal-delivery {
+        padding: 15px 0;
+        font-size: 1.4em;
+        font-weight: bold;
+        img {
+          position: relative;
+          top: -3px;
+          width: 112px;
+        }
+      }
+      .arrival-date {
+        font-size: 1.4em;
+        color: #1e88e5;
+      }
+      .brand {
+        font-size: 1.1em;
+        font-weight: bold;
+      }
+      .product-wrap {
+        display: flex;
+        margin: auto 0;
+        ul {
+          margin: 0;
+          padding: 0;
+        }
+        img {
+          width: 95px;
+          height: 95px;
+        }
+        .grey {
+          color: #9e9e9e;
+        }
+      }
+      .price {
+        padding-right: 60px;
+        font-size: 1.05em;
+      }
+      .price-value {
+        padding-right: 60px;
+        font-weight: bold;
+      }
+    }
+  }
+}
+.total-price-wrap {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 30px;
+  font-size: 1.8em;
+  font-weight: bold;
+  .total-price-value {
+    color: #ff204b;
+  }
+}
+</style>
---
+++ service_front/src/components/Order/OrdererInfo.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="orderer-info-wrap">
+    <h2 class="orderer-info-title">주문자 정보</h2>
+    <div class="orderer-info-table">
+      <div class="name-row">
+        <span class="name">이름</span>
+        <span class="name-input"
+          ><input v-model="ordererlist.name" type="text" placeholder="이름"
+        /></span>
+      </div>
+      <div class="phone-row">
+        <span class="phone">휴대폰</span>
+        <span class="phone-input"
+          ><div class="phone-wrap">
+            <input
+              v-model="ordererlist.phone1"
+              type="number"
+            />&nbsp;-&nbsp;<input
+              v-model="ordererlist.phone2"
+              type="number"
+            />&nbsp;-&nbsp;<input
+              v-model="ordererlist.phone3"
+              type="number"
+            />&nbsp;&nbsp;<button type="button" class="get-vertify">
+              인증번호받기
+            </button>
+          </div>
+          <div class="attention">
+            * 주문자의 휴대폰 번호로 본인 인증 후 주문 가능합니다.<br />
+            * 비회원 주문 시, 적립금은 지급되지 않습니다.
+          </div>
+          <div class="vertify-wrap">
+            <input
+              v-model="ordererlist.vertifynum"
+              class="vertify"
+              type="number"
+              placeholder="인증번호 숫자 6자리"
+            />&nbsp;&nbsp;<button class="confirm" type="button">확인</button>
+          </div></span
+        >
+      </div>
+      <div class="email-row">
+        <span class="email">이메일</span>
+        <span class="email-input"
+          ><input
+            v-model="ordererlist.email1"
+            type="email" />&nbsp;@&nbsp;<input
+            v-model="ordererlist.email2"
+            type="email"
+        /></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["ordererlist"],
+};
+</script>
+
+<style lang="scss">
+.orderer-info-wrap {
+  input {
+    padding: 13px;
+    color: #8d8d8d;
+    background-color: #f5f5f5;
+    &::placeholder {
+      font-size: 0.9em;
+    }
+  }
+  .orderer-info-title {
+    margin-top: 90px;
+    padding-bottom: 10px;
+    font-size: 1.8em;
+    border-bottom: 1px solid black;
+  }
+  .orderer-info-table {
+    border-bottom: 1px solid black;
+    span {
+      margin: auto 0;
+      padding: 15px;
+    }
+    .name-row,
+    .phone-row,
+    .email-row {
+      display: flex;
+      justify-content: end;
+      border-bottom: 1px solid #bdbdbd;
+      .name {
+        width: 221px;
+        font-weight: bold;
+      }
+      .name-input {
+        width: 968px;
+        input {
+          width: 105%;
+          height: 42px;
+        }
+      }
+      .phone {
+        width: 221px;
+        font-weight: bold;
+      }
+      .phone-input {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        width: 968px;
+        height: 190px;
+        input {
+          width: 80px;
+          height: 42px;
+          text-align: center;
+        }
+        button {
+          width: 94px;
+          height: 42px;
+          color: white;
+          font-size: 0.8em;
+          font-weight: bold;
+        }
+        .get-vertify {
+          background-color: #4c4c4c;
+        }
+        .confirm {
+          background-color: #bdbdbd;
+        }
+        .attention {
+          font-size: 0.85em;
+        }
+        .vertify {
+          width: 238px;
+        }
+      }
+      .email {
+        width: 221px;
+        font-weight: bold;
+      }
+      .email-input {
+        width: 968px;
+        input {
+          width: 238px;
+          height: 42px;
+        }
+      }
+    }
+  }
+}
+</style>
